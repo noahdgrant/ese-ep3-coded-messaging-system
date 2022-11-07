0f1a069a4dade58c13f3cccdcbdb4ff520931f6d
noahdgrant <105687410+noahdgrant@users.noreply.github.com>
2022-11-07 15:23:28 -0500

    Update main.cpp
    Fixed one merge conflict

--- main.cpp
+++ main.cpp
@@ -59,7 +59,7 @@ int	main(int argc, char* argv[])
 				Sleep(4000);
 				break;
 			// Transmit Text Message
-			case '5':
+			case 5:
 				printf("\nWhat message would you link to send?\n\n");
 				fflush(stdin);													// Flush input buffer after use. Good practice in C
 				scanf_s("%[^\n]s", msg, sizeof(msg));						// Reading complete strings with scanf_s: https://www.geeksforgeeks.org/difference-between-scanf-and-gets-in-c/
@@ -69,12 +69,12 @@ int	main(int argc, char* argv[])
 				Sleep(4000);
 				break;
 			// Recieve Text Message
-			case '6':
+			case 6:
 				receiveCom();
 				Sleep(4000);
 				break;
 			// Transmit audio message
-			case '7':
+			case 7:
 				// RECORD MESSAGE
 				// initialize playback and recording
 				InitializeRecording();
@@ -99,7 +99,7 @@ int	main(int argc, char* argv[])
 				}
 				break;
 			// Recieve audio message
-			case '8':
+			case 8:
 				receiveCom();
 				Sleep(4000);
 				break;
